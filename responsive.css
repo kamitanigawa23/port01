134510979eb8f7e2b98bb2a451e09d1ee665e1cd
kamitanigawa23 <69797949+kamitanigawa23@users.noreply.github.com>
2021-01-24 13:57:03 +0900

    Create responsive.css

---
+++ responsive.css
@@ -0,0 +1,54 @@
+@media(max-width:780px){
+  *{
+    box-sizing: border-box;
+  }
+  .head{
+    width: 50%;
+  }
+  .header-left a{
+    font-size: 1rem;
+  }
+  .main-nav a{
+    font-size: 12px;
+  }
+  .about{
+    font-size: 0.8rem;
+  }
+  .main-2{
+    flex-direction: column;
+    align-items: center;
+    /* background-color: burlywood; */
+    width: 98%;
+    margin: 0 auto;
+  }
+  .main-right h2{
+    font-size: 0.5rem;
+    padding-top: 10%;
+    text-align: center;
+  }
+  .main-right p{
+    font-size: 0.25rem;
+    text-align: center;
+    width: 100%;
+    /* margin-left: 3%; */
+    /* background-color: burlywood; */
+  }
+  .bicycle{
+    font-size: 0.8rem;
+  }
+  .chapter{
+    flex-direction: column;
+    align-items: center;
+  }
+  .tile{
+    padding-bottom: 5%;
+  }
+}
+@media(max-width:1000px){
+  *{
+    box-sizing: border-box;
+  }
+  .chapter h2{
+    font-size: 1.3rem;
+  }
+}
